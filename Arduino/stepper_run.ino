022aaddcf3ed39d9f16f72aa0f39ca15c6c290a3
poloko <30334192+poloko159@users.noreply.github.com>
2018-09-28 15:15:05 +0700

    works

---
+++ Arduino/stepper_run.ino
@@ -0,0 +1,41 @@
+void stepper_run( uint8_t motorNumber, float motor_speed)
+{ 
+  bool dir;  //motor direction
+  uint16_t PWM_count;
+
+  //determine needed motor direction and make speed pos.
+  if (motor_speed < 0)
+  {
+    dir = 1;
+    motor_speed = abs(motor_speed);
+  }
+  else dir = 0;
+
+  if (motor_speed > 10 )
+  {
+    PWM_count =15625/motor_speed;  //calculate needed compare value for speed
+  }
+  else PWM_count = 15625000000;  //set compare value to inf to keep stepper output from toggling when zero velocity needed
+
+  if (motorNumber == 1) //motor a-------------------------------------------
+  {
+    digitalWrite(dir_a_pin, dir);
+    dir_a = dir;
+    OCR4A = PWM_count;  //set motor a step speed compare register
+  }
+
+  if (motorNumber == 2) //motor b----------------------------------------------
+  {
+    digitalWrite(dir_b_pin, dir);
+    dir_b = dir;
+    OCR3A = PWM_count; //set motor b step speed compare register
+  }
+
+  if (motorNumber == 3) //motor c-----------------------------------------------
+  {
+    digitalWrite(dir_c_pin, dir);
+    dir_c = dir;
+    OCR5A = PWM_count; //set motor c step speed compare register
+  }
+  
+}
---
+++ Matlab/trackball.m
@@ -0,0 +1,37 @@
+function [img, bw,aa,bb] = trackball(img, thresh)
+% TRACKBALL
+
+%% Extract RGB color components
+r = img(:,:,1);
+g = img(:,:,2);
+b = img(:,:,3);     
+aa = 0;
+bb = 0;
+SE = strel('rectangle',[7 7]);
+
+
+%% Calculate green
+justGreen = g - r/2 - b/2;
+
+%% Threshold the image
+bw = justGreen > thresh;
+
+%% Find center
+[x, y] = find(bw);
+if ~isempty(x) && ~isempty(y)
+    xm = round(mean(x));
+    ym = round(mean(y));
+    
+    %% Creat a red dot
+    xx = max(1, xm-5):min(xm+5, size(bw, 1));
+    yy = max(1, ym-5):min(ym+5, size(bw, 2));
+    bwbw = zeros(size(bw), 'uint8');
+    bwbw(xx, yy) = 255;
+    bb = -xm+240/2;
+    aa = ym-320/2;
+    %% Create output image
+    img(:,:,1) = uint8(r + bwbw);
+    img(:,:,2) = uint8(g - bwbw);
+    img(:,:,3) = uint8(b - bwbw);
+end
+
